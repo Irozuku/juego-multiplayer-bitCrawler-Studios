afabe0456ec4a1b68fe91add84fa253cd19cc0a2
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-08-30 12:47:11 -0400

    Update main.gd

--- scenes/main.gd
+++ scenes/main.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@onready var p1 = $Player1
+@onready var p2 = $Player2
+
+func _ready():
+	var player = Game.get_current_player()
+	if player.role == 1:
+		p1.setup(player)
+	if player.role == 2:
+		p2.setup(player)
