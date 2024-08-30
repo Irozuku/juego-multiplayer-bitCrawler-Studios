9211c8a08e2e441213437df7af0dd285b42f6384
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-08-30 13:28:56 -0400

    Added logic for abilities

--- scenes/entity/player1/player_1.gd
+++ scenes/entity/player1/player_1.gd
@@ -1,1 +1,10 @@
 extends PlayerBase
+
+@onready var hammer = $Hammer
+
+func _ready():
+	jump_speed = 700
+
+#when hammer1 pressed, create a collision shape, if that collision hits a breakable wall, break
+
+#when hammer2 pressed, make a greater jump, if in contact with p2, make him jumo too
--- scenes/entity/player2/player_2.gd
+++ scenes/entity/player2/player_2.gd
@@ -1,1 +1,7 @@
 extends PlayerBase
+
+#when chain pressed, send a line.
+	#If contact with hook, make swing.
+		#allow chain pressed to let go
+	#If contact with p1, grab
+		#allow roll with wheel to drag player and chain pressed to let go
