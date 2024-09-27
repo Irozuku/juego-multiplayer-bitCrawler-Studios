094958c533e1968ba2d3e2669cb213d13b4272f9
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-09-27 13:34:16 -0300

    Added Breakable object and hammer logic

---
+++ scenes/breakables/breakable.gd
@@ -0,0 +1,6 @@
+extends Area2D
+
+func destroy():
+	#play anim
+	queue_free()
+	
--- scenes/entity/player1/player_1.gd
+++ scenes/entity/player1/player_1.gd
@@ -2,17 +2,43 @@ extends PlayerBase
 
 @onready var hammer = $Hammer
 @onready var animation_tree : AnimationTree = $AnimationTree
+var breakable = null
 
 func _ready():
 	animation_tree.active = true
 	jump_speed = 700
 
-func _process(delta):
+func _physics_process(delta: float) -> void:
+	super(delta)
 	update_animation()
+	if is_multiplayer_authority():
+		var move_input = Input.get_axis("move_left", "move_right")
+		if move_input < 0:
+			print("flip")
+		if move_input > 0:
+			print("flip")
 
-#when hammer1 pressed, create a collision shape, if that collision hits a breakable wall, break
+func _input(event: InputEvent) -> void:
+	if is_multiplayer_authority():
+		if Input.is_action_just_pressed("hammer1"):
+			#play anim
+			check_breakable()
+		#if Input.is_action_just_pressed("hammer2"):
+			#when hammer2 pressed, make a greater jump, if in contact with p2, make him jumo too
+			#return
 
-#when hammer2 pressed, make a greater jump, if in contact with p2, make him jumo too
+@rpc("any_peer", "call_local", "reliable")
+func check_breakable():
+	if (breakable != null):
+		breakable.destroy()
+
+func _on_hammer_area_entered(area):
+	if area.is_in_group("breakable"):
+		breakable = area
+
+func _on_hammer_area_exited(area):
+	if area.is_in_group("breakable"):
+		breakable = null
 
 func update_animation():
 	if(velocity == Vector2.ZERO):
