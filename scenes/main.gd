7305d256c31c3f509ee8533c1ea835aff71f1f61
andrea-tri <aptrigod@gmail.com>
2024-09-27 12:11:56 -0300

    Mapa y assests

--- scenes/main.gd
+++ scenes/main.gd
@@ -2,10 +2,16 @@ extends Node2D
 
 @onready var p1 = $Player1
 @onready var p2 = $Player2
+@onready var camera1 = $Player1/Camera2D
+@onready var camera2 = $Player2/Camera2D
 
 func _ready():
 	for player in Game.players:
 		if player.role == 1:
 			p1.setup(player)
+			if player.id == multiplayer.get_unique_id():
+				camera1.make_current()
 		if player.role == 2:
 			p2.setup(player)
+			if player.id == multiplayer.get_unique_id():
+				camera2.make_current()
