43e5d8c6768d1a1bc79ed9dcba2f0fac9a0956dd
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-10-11 12:58:52 -0300

    Added break animation

--- scenes/breakables/breakable.gd
+++ scenes/breakables/breakable.gd
@@ -1,5 +1,11 @@
 extends StaticBody2D
+@onready var animation_player = $AnimationPlayer
 
 func destroy():
-	#play anim
-	queue_free()
+	animation_player.play("Break")
+	
+
+
+func _on_animation_player_animation_finished(anim_name):
+	if anim_name == "Break":
+		queue_free()
