d36d771aec2432a3b8798015a4506d03ea56caf0
Sebastian Saez <sebastian.saez@ug.uchile.cl>
2024-11-28 17:18:09 -0300

    Bug fix

--- scenes/breakables/breakable.gd
+++ scenes/breakables/breakable.gd
@@ -1,8 +1,10 @@
 extends StaticBody2D
 @onready var animation_player = $AnimationPlayer
+@onready var collision_shape_2d = $CollisionShape2D
 
 func destroy():
 	animation_player.play("Break")
+	collision_shape_2d.set_deferred("disabled",true)
 	
 
 
